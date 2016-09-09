418ccae8889fbf1b8d1d626f49aa606c989b0441
Robert Bunch <rob@digitalcrafts.com>
2016-09-09 15:43:04 -0400

    init repo - angular and Express

---
+++ front-end/controller.js
@@ -0,0 +1,5 @@
+var searchStudentApp = angular.module('searchStudentApp', []);
+
+searchStudentApp.controller('searchStudentController', function($scope, $http){
+	$scope.message = "THis is a test";
+});
